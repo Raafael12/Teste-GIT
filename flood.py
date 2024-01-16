2b7277f236d829c4f4707d918eba3135d9e68281
Rafael Magalhaes <rafar20082004@gmail.com>
2024-01-16 10:02:24 -0300

    Commit inicial

---
+++ flood.py
@@ -0,0 +1,20 @@
+import pyautogui as ag
+from time import sleep
+
+# mudança pra commit aa
+
+vai = True
+iter = 0
+print('Contagem de 3')
+sleep(3)
+print('Init')
+ag.write('Oi')
+ag.press('enter')
+sleep(10)
+while vai:
+    print(iter)
+    ag.write('mor')
+    ag.press('enter')
+    iter += 1
+    if iter >= 10:
+        break
